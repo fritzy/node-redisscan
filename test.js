c5bb97d27e3897f759ba17b645d92ee59f6e35f2
EyePulp <eyepulp@eyepulp.net>
2017-04-05 18:41:47 -0500

    fix the test callback to account for the length variable

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var redis       = require('redis').createClient();
 
 redisScan({
     redis: redis,
-    each_callback: function (type, key, subkey, value, cb) {
-        console.log(type, key, subkey, value);
+    each_callback: function (type, key, subkey, length, value, cb) {
+        console.log(type, key, subkey, length, value);
         cb();
     },
     done_callback: function (err) {
